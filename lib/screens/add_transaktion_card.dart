812b69e498c108e4f13854662c75ceb2998537d1
fdufter <florian.dufter@tum.de>
2020-03-22 13:31:57 +0100

    added name eingeben field, polishment

--- lib/screens/add_transaktion_card.dart
+++ lib/screens/add_transaktion_card.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class AddTransaktionCard extends StatelessWidget {
-  final TextEditingController controller = new TextEditingController();
-
+  final TextEditingController betragController = new TextEditingController();
+  final TextEditingController nameController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -12,14 +12,10 @@ class AddTransaktionCard extends StatelessWidget {
         decoration: BoxDecoration(
           boxShadow: [
             BoxShadow(
-              color: Colors.grey,
-              blurRadius: 20.0,
-              spreadRadius: 3.0,
-              offset: Offset(
-                7.0,
-                7.0
-              )
-            )
+                color: Colors.grey,
+                blurRadius: 20.0,
+                spreadRadius: 3.0,
+                offset: Offset(7.0, 7.0))
           ],
           borderRadius: BorderRadius.circular(20),
           color: Colors.white,
@@ -28,11 +24,34 @@ class AddTransaktionCard extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: <Widget>[
-              TextField(
-                controller: controller,
-                style: TextStyle(color: Colors.white, fontSize: 24),
-                decoration: InputDecoration(
-                    border: InputBorder.none, hintText: 'Betrag eingeben'),
+              Text(
+                "Transaktion hinzufügen",
+                style: TextStyle(fontSize: 22),
+              ),
+              SizedBox(height: 10,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Betrag eingeben"),
+                  TextField(
+                    controller: betragController,
+                    style: TextStyle(fontSize: 18),
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: 'Betrag eingeben'),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Name eingeben"),
+                  TextField(
+                    controller: nameController,
+                    style: TextStyle(fontSize: 18),
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: 'Name eingeben'),
+                  ),
+                ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
